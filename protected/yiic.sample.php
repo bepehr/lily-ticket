90ab7be861cfbb8c59e782f3efd6180fb3ecaa0b
etherfoundry <gpgarrettboast@gmail.com>
2013-09-14 20:54:56 -0700

    Base Project commit

---
+++ protected/yiic.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../path/to/yiiframework/yii.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
